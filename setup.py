4c9d958124069efaa305ddaf0d26482c9dd8c8ca
Ryan Rueger <rrueger@dynaikon.com>
2022-06-05 16:20:57 +0200

    Switch to setup.py standard from setup.cfg
    We need different tflite_runtime dependencies for the Raspberry Pi Zero
    and other platforms.
    
    The setup.cfg standard (although not officially recommended for new
    packages [1]) doesn't allow for any setup logic (which is one of the
    purported benefits).
    
    The setup.py standard allows us to decide at installation time which
    tflite_runtime to pull.
    
    Although modern documentation suggests that one can still use setup.cfg
    for the declarative parts, and then setup.py for the non-supported parts
    [1], I have chosen to only use the setup.py standard for now.
    
    Links:
    
      [1] https://setuptools.pypa.io/en/latest/userguide/quickstart.html#setup-py

---
+++ setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+def get_platform():
+    try:
+        with open("/sys/firmware/devicetree/base/model", "r") as f:
+            model = "".join(f.readline().strip().split()[:3])
+    except Exception:
+        model = ""
+
+    return model
+
+if get_platform() == "Raspberry Pi Zero":
+    tflite_dependency = 'tflite_runtime @ https://dynaikon.com/resources/tflite_runtime-2.7.0-cp37-cp37m-linux_armv6l.whl'
+else:
+    tflite_dependency = 'tflite_runtime @ https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp37-cp37m-linux_armv7l.whl'
+
+with open("DynAIkonTrap/VERSION", "r") as f:
+    version = f.read()
+
+setup(
+    name='DynAIkonTrap',
+    version=version,
+    packages=find_packages(where="."),
+    install_requires=[
+        'picamera==1.13',
+        'pyserial==3.4',
+        'requests==2.24.0',
+        'urllib3==1.25.9',
+        'opencv-python-headless==4.4.0.44',
+        'scipy',
+        'Pillow==8.4.0',
+        tflite_dependency,
+    ],
+    entry_points={
+        'console_scripts': ['dynaikontrap=DynAIkonTrap.__main__:main']
+    },
+    package_data={
+        "": [
+            'VERSION',
+            'filtering/yolo_animal_detector.cfg',
+            'filtering/yolo_animal_detector.weights',
+            'filtering/yolo_animal_detector.weights',
+            'filtering/models/ssdlite_mobilenet_v2_animal_human/model.tflite',
+            'filtering/models/ssdlite_mobilenet_v2_animal_only/model.tflite',
+            'filtering/models/ssdlite_mobilenet_v2_animal_only/model2.tflite',
+            ]
+    }
+)
