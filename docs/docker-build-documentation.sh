543ca5e0935f65e369828f7473f8e3e0601be6d7
Ryan Rueger <rrueger@dynaikon.com>
2022-10-27 23:27:25 +0100

    [docs] Make the documentation building container continuous
    Now the documentation building docker container watches the source
    repository for changes and continuously rebuilds the documentation on
    every change.

--- docs/docker-build-documentation.sh
+++ docs/docker-build-documentation.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
 
 repo=$1
-output=$2
 
-if [[ $repo == '' ]] || [[ ! -d $(readlink -f "$repo") ]] || [[ $output == '' ]]
+if [[ $repo == '' ]] || [[ ! -d $(readlink -f "$repo") ]]
 then
-  echo "Usage: docker-build.sh <path to git repository root> <output directory>"
+  echo "Usage: docker-build.sh <path to git repository root>"
   exit 2
 fi
 
+echo "This script will use a docker container to build the documentation"
+echo "It will watch '$repo' and continuously re-build docs when files change"
+
+# Get Userid and Groupid so that the files can be set to the correct owner
+USERSTRING=$(id -u):$(id -g)
+
 # Use readlink to canonicalise paths
-sudo docker run --rm                         \
-  -v "$(readlink -f "$repo")":/repository:ro \
-  -v "$(readlink -f "$output")":/output      \
-  python:3.7                                 \
-  bash -O dotglob -o errexit -c '
-    mkdir /tmp/build
-    cp -a /repository/* /tmp/build
-    cd /tmp/build/docs
+sudo docker run -it --rm                  \
+  -v "$(readlink -f "$repo")":/repository \
+  python:3.7                              \
+  bash -O dotglob -o errexit -c "
+    apt update -qqy
+    apt upgrade -qqy
+    apt install -qqy inotify-tools
+    cd /repository/docs
     export READTHEDOCS=True
     pip install -r requirements.txt
     pip install -r ../requirements.txt
+
     make clean
     make html
-    cp -a build/html/* /output
-    '
-
-# Since the files were created by root operating docker, they belong to root
-sudo chown "$USER:$USER" -R "$(readlink -f "$output")"
+    while inotifywait -e modify,create -r /repository
+    do
+      make html
+      chown \"$USERSTRING\" -R /repository/docs/build
+    done
+    "
