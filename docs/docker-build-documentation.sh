fe6f9d24de2044e7064ec9f0b8c2e8a9c5d6d8bb
Ryan Rueger <rrueger@dynaikon.com>
2022-12-26 21:53:16 +0000

    [docs] Make documentation builder more verbose
    Now, it will explain what is happening to the user, and offer an option
    to exit. It will also explain how to view the documentation locally.

--- docs/docker-build-documentation.sh
+++ docs/docker-build-documentation.sh
@@ -8,16 +8,47 @@ then
   exit 2
 fi
 
-echo "This script will use a docker container to build the documentation"
-echo "It will watch '$repo' and continuously re-build docs when files change"
+# Use readlink to canonicalise paths
+repo=$(readlink -f "$repo")
+
+cat <<EOF
+--------------------------------------------------------------------------------
+DynAikonTrap documentation builder
+--------------------------------------------------------------------------------
+This script will spawn a dockere container to continuously build the
+documentation in the directory 'docs' from the repository
+
+$repo
+
+On any file change, the Documentation will be re-built.
+
+To view the documentation, point your browser to docs/build/html/index.html
+
+e.g.
+$ firefox file://$repo/docs/build/html/index.html
+
+Initialising this container will take around 2 minutes (depending on your
+internet connection).
+
+To launch this script, your user must be able to access the docker daemon (via
+sudo).
+--------------------------------------------------------------------------------
+EOF
+
+read -rn1 -p 'Press y|Y to continue: '
+if ! [[ $REPLY =~ y|Y ]]
+then
+  echo
+  echo "Okay, exiting..."
+  exit 1
+fi
 
 # Get Userid and Groupid so that the files can be set to the correct owner
 USERSTRING=$(id -u):$(id -g)
 
-# Use readlink to canonicalise paths
-sudo docker run -it --rm                  \
-  -v "$(readlink -f "$repo")":/repository \
-  python:3.7                              \
+sudo docker run -it --rm \
+  -v "$repo":/repository \
+  python:3.7             \
   bash -O dotglob -o errexit -c "
     apt update -qqy
     apt upgrade -qqy
